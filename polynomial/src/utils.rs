de6c153b3b101c7c141bdba6c82a29654b32693a
Nwele Uchenna (developeruche) <nweleuchenna2004@gmail.com>
2024-05-20 17:10:15 +0100

    utils test

--- polynomial/src/utils.rs
+++ polynomial/src/utils.rs
@@ -46,3 +46,36 @@ pub fn get_langrange_basis<F: Field>(
 pub fn multilinear_evalutation_equation<F: Field>(x: F, y_1: F, y_2: F) -> F {
     x * y_2 + (F::one() - x) * y_1
 }
+
+/// returns a vector of (y_1, y_2)
+pub fn round_pairing_index(len: usize) ->Vec<(usize, usize)> {
+    let mut result = Vec::new();
+    for y_1 in 0..len / 2 {
+        result.push((y_1, (len / 2) + y_1));
+    }
+
+    result
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use ark_test_curves::bls12_381::Fr;
+
+    #[test]
+    fn test_multilinear_evalutation_equation() {
+        let x = Fr::from(5);
+        let y_1 = Fr::from(3);
+        let y_2 = Fr::from(2);
+
+        assert_eq!(multilinear_evalutation_equation(x, y_1, y_2), Fr::from(-2));
+    }
+
+    #[test]
+    fn test_round_pairing_index() {
+        let len = 4;
+        let result = round_pairing_index(len);
+        assert_eq!(result, vec![(0, 2), (1, 3)]);
+    }
+}
