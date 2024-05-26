3bb43105bb37ba9015ffadb5f8cd427352337a1e
Nwele Uchenna (developeruche) <nweleuchenna2004@gmail.com>
2024-05-26 16:21:22 +0100

    mod: done interfacing the verifier

--- sum_check/src/verifier.rs
+++ sum_check/src/verifier.rs
@@ -1,1 +1,35 @@
+use ark_ff::Field;
+use fiat_shamir::FiatShamirTranscript;
+use crate::{data_structure::SumCheckProof, interface::VerifierInterface};
 
+
+
+
+
+
+#[derive(Clone, Default, Debug)]
+pub struct Verifier<F: Field> {
+    /// This is this fiat-shamir challenge transcript
+    pub transcript: FiatShamirTranscript,
+    phantom: std::marker::PhantomData<F>,
+}
+
+
+
+impl<F: Field> Verifier<F> {
+    /// This function creates a new verifier instance
+    pub fn new() -> Self {
+        Self {
+            transcript: FiatShamirTranscript::default(),
+            phantom: std::marker::PhantomData,
+        }
+    }
+}
+
+
+impl<F: Field> VerifierInterface<F> for Verifier<F> {
+    /// This function verifies the sum check proof
+    fn verify(&self, proof: &SumCheckProof<F>) -> bool {
+        unimplemented!("Implement this function")
+    }
+}
