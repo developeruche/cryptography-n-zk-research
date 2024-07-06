2256fbf4a272fc81f40f3d5558f424f5515f202a
Nwele Uchenna (developeruche) <nweleuchenna2004@gmail.com>
2024-07-06 15:20:04 +0100

    mod: working on circom-groth16

---
+++ circom-groth16/src/main.rs
@@ -0,0 +1,10 @@
+use ark_circom::{CircomBuilder, CircomConfig};
+use ark_std::rand::thread_rng;
+use ark_bn254::Bn254;
+
+
+// type Groth16Protocol = groth16::protocol::Groth16Protocol<Bn254>;
+
+fn main() {
+    
+}
