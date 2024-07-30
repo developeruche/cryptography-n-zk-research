e1df1877d3c709e7b6f8a69cf13e332e2c3deaeb
Nwele Uchenna (developeruche) <nweleuchenna2004@gmail.com>
2024-07-30 23:05:49 +0100

    mod: tested evaluation

--- polynomial/src/composed/multilinear.rs
+++ polynomial/src/composed/multilinear.rs
@@ -1,4 +1,5 @@
 use crate::interface::MultilinearPolynomialInterface;
+use crate::multilinear::Multilinear;
 use ark_ff::PrimeField;
 
 /// This is a composition of multilinear polynomials whose binding operation is multiplication
@@ -20,7 +21,7 @@ impl<F: PrimeField> ComposedMultilinear<F> {
 
 impl<F: PrimeField> MultilinearPolynomialInterface<F> for ComposedMultilinear<F> {
     fn num_vars(&self) -> usize {
-        todo!()
+        self.polys[0].num_vars()
     }
 
     fn partial_evaluation(&self, evaluation_point: F, variable_index: usize) -> Self {
@@ -32,7 +33,17 @@ impl<F: PrimeField> MultilinearPolynomialInterface<F> for ComposedMultilinear<F>
     }
 
     fn evaluate(&self, point: &Vec<F>) -> Option<F> {
-        todo!()
+        let mut result = F::one();
+
+        for poly in &self.polys {
+            let eval = poly.evaluate(point);
+            match eval {
+                Some(val) => result *= val,
+                None => return None,
+            }
+        }
+
+        Some(result)
     }
 
     fn extend_with_new_variables(&self, num_of_new_variables: usize) -> Self {
@@ -78,5 +89,13 @@ mod tests {
     use ark_test_curves::bls12_381::Fr;
 
     #[test]
-    fn test_evaluation() {}
+    fn test_evaluation() {
+        let poly1 = Multilinear::new(vec![Fr::from(0), Fr::from(1), Fr::from(2), Fr::from(3)], 2);
+        let poly2 = Multilinear::new(vec![Fr::from(0), Fr::from(0), Fr::from(0), Fr::from(1)], 2);
+
+        let composed = ComposedMultilinear::new(vec![poly1, poly2]);
+
+        let eval = composed.evaluate(&vec![Fr::from(2), Fr::from(3)]);
+        assert_eq!(eval, Some(Fr::from(42)));
+    }
 }
