8398a1009c84a30d16fb4c38b6c5a9829c60d290
Nwele Uchenna (developeruche) <nweleuchenna2004@gmail.com>
2024-10-03 15:56:33 +0100

    mod: transript_append with label

--- fiat_shamir/src/interface.rs
+++ fiat_shamir/src/interface.rs
@@ -4,6 +4,12 @@ use ark_ff::PrimeField;
 pub trait TranscriptInterface {
     /// This is used to append a message to the transcript.
     fn append(&mut self, msg: Vec<u8>);
+    /// This is used to appended a message to the transcript alongside a label
+    fn append_with_label(&mut self, label: &str, msg: Vec<u8>) {
+        let mut label_bytes = label.as_bytes().to_vec();
+        label_bytes.append(&mut msg.clone());
+        self.append(label_bytes);
+    }
     /// This is used to get the challenge from the transcript.
     fn sample(&mut self) -> [u8; 32];
     /// This is used to sample n challenges from the transcript.
