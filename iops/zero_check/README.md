6e4750333544f5014a70b07fa73ddac286f88299
developeruche <developeruche@gmail.com>
2025-03-16 12:50:33 +0100

    mod: done with trait installation for zero check

---
+++ iops/zero_check/README.md
@@ -0,0 +1,7 @@
+# Zero Check Protocol
+
+The ZeroCheck PIOP (Polynomial Interactive Oracle Protocol) is a subprotocol used in zero-knowledge proof systems to verify that a polynomial evaluates to zero over a specific domain. It is a critical building block in many zero-knowledge proof systems, particularly in protocols like Plonk, to check consistency or enforce certain constraints.
+
+
+Acknowledgment
+https://github.com/EspressoSystems/hyperplonk
--- iops/zero_check/src/lib.rs
+++ iops/zero_check/src/lib.rs
@@ -1,2 +1,35 @@
+use ark_ff::PrimeField;
+use fiat_shamir::FiatShamirTranscript;
+use interface::ZeroCheckInterface;
+use polynomial::composed::multilinear::ComposedMultilinear;
+use std::marker::PhantomData;
+use sum_check::composed::ComposedSumCheckProof;
 pub mod interface;
 pub mod primitives;
+
+/// Struct used to create a instance of the zero check protocol.
+pub struct ZeroCheck<F: PrimeField> {
+    _phantom: PhantomData<F>,
+}
+
+impl<F: PrimeField> ZeroCheckInterface for ZeroCheck<F> {
+    type Poly = ComposedMultilinear<F>;
+    type SubClaim = primitives::ZeroCheckSubClaim<F>;
+    type Proof = ComposedSumCheckProof<F>;
+    type Transcript = FiatShamirTranscript;
+
+    fn prove(
+        poly: &Self::Poly,
+        transcript: &mut Self::Transcript,
+    ) -> Result<Self::Proof, anyhow::Error> {
+        todo!()
+    }
+
+    fn verify(
+        proof: &Self::Proof,
+        poly: &Self::Poly,
+        transcript: &mut Self::Transcript,
+    ) -> Result<bool, anyhow::Error> {
+        todo!()
+    }
+}
