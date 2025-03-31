4756cfa2db1a5396edc2799a7921fde7875aaeb1
developeruche <developeruche@gmail.com>
2025-03-31 14:07:50 +0100

    init sumcheck201

--- iops/sumcheck201playground/README.md
+++ iops/sumcheck201playground/README.md
@@ -1,1 +1,3 @@
 # Sumcheck 201 Playground
+
+This section of this research repo is focused on experimenting with the Sumcheck201 algorithm.
--- iops/sumcheck201playground/src/lib.rs
+++ iops/sumcheck201playground/src/lib.rs
@@ -1,14 +1,5 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub mod a1_prover;
+pub mod a2_prover;
+pub mod interfaces;
+pub mod primitives;
+pub mod verifier;
