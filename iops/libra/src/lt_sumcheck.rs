c38f7304ee8b441e70698662fff5f1bfac12e302
developeruche <developeruche@gmail.com>
2025-06-01 18:44:34 +0100

    done with the sum check protocol

--- iops/libra/src/lt_sumcheck.rs
+++ iops/libra/src/lt_sumcheck.rs
@@ -1,14 +1,18 @@
 //! This is an implemention of a linear time sum check protocol.
 //! as discribed in the Libra paper.
-use std::rc::Rc;
 use p3_field::{ExtensionField, Field, PrimeField32};
-use poly::{Fields, vpoly::VPoly};
+use poly::{Fields, MultilinearExtension, mle::MultilinearPoly, vpoly::VPoly};
+use std::rc::Rc;
 use sum_check::{SumCheck, interface::SumCheckInterface};
 use transcript::Transcript;
 
-use crate::{algos::initialize_phase_1, utils::product_combined_fn};
+use crate::{
+    algos::{initialize_phase_1, initialize_phase_2},
+    utils::{generate_igz, product_combined_fn},
+};
 
 type PartialSumCheckProof<F, E> = (Vec<Vec<Fields<F, E>>>, Vec<E>);
+type SumCheckProof<F, E> = Vec<Vec<Fields<F, E>>>;
 
 /// A trait for a linear time sum check protocol.
 pub trait LinearTimeSumCheckTr<F: Field + PrimeField32, E: ExtensionField<F>> {
@@ -26,8 +30,31 @@ pub trait LinearTimeSumCheckTr<F: Field + PrimeField32, E: ExtensionField<F>> {
         i_gz: &[E],
         transcript: &mut Transcript<F, E>,
     ) -> Result<PartialSumCheckProof<F, E>, anyhow::Error>;
-    fn phase_two();
-    fn sum_check();
+
+    /// phase two houses two algorithms
+    /// 1. initialize: Creating the A_f_1 book-keeping table using Algorithm 5
+    /// 2. sum_check_product: Performing the sum check product operation using the sl-core library
+    ///
+    /// params:
+    /// f_1: &[(usize, usize, usize)] :> This is a sparse polynomial in GKR protocol, this could be the add_mle or mul_mle
+    /// i_gz: &[E] :> This is the i(g,z) table generated from `g`.
+    /// i_uz: &[E] :> This is the i(u,z) table generated from `u`.
+    fn phase_two(
+        f_1: &[(usize, usize, usize)],
+        f_2_3: &[F],
+        i_gz: &[E],
+        i_uz: &[E],
+        f2_u: &E,
+        transcript: &mut Transcript<F, E>,
+    ) -> Result<PartialSumCheckProof<F, E>, anyhow::Error>;
+
+    /// Sum-check a combination of two phases.
+    fn sum_check(
+        f_1: &[(usize, usize, usize)],
+        f_2_3: &[F],
+        i_gz: &[E],
+        transcript: &mut Transcript<F, E>,
+    ) -> Result<SumCheckProof<F, E>, anyhow::Error>;
 }
 
 /// A struct for a linear time sum check protocol.
@@ -46,16 +73,73 @@ impl<F: Field + PrimeField32, E: ExtensionField<F>> LinearTimeSumCheckTr<F, E>
     ) -> Result<PartialSumCheckProof<F, E>, anyhow::Error> {
         let a_hg = initialize_phase_1(f_1, f_2_3, i_gz);
         let num_var = (a_hg.len() as f64).log2() as usize;
-        let product_poly = VPoly::new(vec![], 2, num_var, Rc::new(product_combined_fn));
+
+        let a_hg_mle = MultilinearPoly::new_from_vec(
+            num_var,
+            a_hg.iter()
+                .map(|&x| Fields::Extension(x))
+                .collect::<Vec<_>>(),
+        );
+        let f_2_3_mle = MultilinearPoly::new_from_vec(
+            num_var,
+            f_2_3.iter().map(|&x| Fields::Base(x)).collect::<Vec<_>>(),
+        );
+        let product_poly = VPoly::new(
+            vec![a_hg_mle, f_2_3_mle],
+            2,
+            num_var,
+            Rc::new(product_combined_fn),
+        );
 
         SumCheck::prove_partial(&product_poly, transcript)
     }
 
-    fn phase_two() {
-        todo!()
+    fn phase_two(
+        f_1: &[(usize, usize, usize)],
+        f_2_3: &[F],
+        i_gz: &[E],
+        i_uz: &[E],
+        f2_u: &E,
+        transcript: &mut Transcript<F, E>,
+    ) -> Result<PartialSumCheckProof<F, E>, anyhow::Error> {
+        let a_f_1 = initialize_phase_2(f_1, i_gz, i_uz);
+        let num_var = (a_f_1.len() as f64).log2() as usize;
+
+        let f_2_3_vec = &f_2_3.to_vec();
+        let f_2_3_mle: MultilinearPoly<F, E> = f_2_3_vec.into();
+        let f3_y_mul_f2_u = f_2_3_mle * Fields::<F, E>::Extension(*f2_u);
+        let product_poly = VPoly::new(
+            vec![a_f_1.into(), f3_y_mul_f2_u],
+            2,
+            num_var,
+            Rc::new(product_combined_fn),
+        );
+
+        SumCheck::prove_partial(&product_poly, transcript)
     }
 
-    fn sum_check() {
-        todo!()
+    fn sum_check(
+        f_1: &[(usize, usize, usize)],
+        f_2_3: &[F],
+        i_gz: &[E],
+        transcript: &mut Transcript<F, E>,
+    ) -> Result<SumCheckProof<F, E>, anyhow::Error> {
+        let (phase_one_round_polys, u) = Self::phase_one(f_1, f_2_3, i_gz, transcript)?;
+
+        let i_uz = generate_igz::<F, E>(&u);
+        let f2_vec = &f_2_3.to_vec();
+        let f2: MultilinearPoly<F, E> = f2_vec.into();
+        let f2_u = f2.evaluate(&u.iter().map(|&x| Fields::Extension(x)).collect::<Vec<_>>());
+
+        let (phase_two_round_polys, _) = Self::phase_two(
+            f_1,
+            f_2_3,
+            i_gz,
+            &i_uz,
+            &f2_u.to_extension_field(),
+            transcript,
+        )?;
+
+        Ok([phase_one_round_polys, phase_two_round_polys].concat())
     }
 }
