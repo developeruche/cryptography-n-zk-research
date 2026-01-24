85c0f7b02087e8478b3fea31d1fa750a0244b8b0
developeruche <developeruche@gmail.com>
2026-01-24 21:53:30 +0100

    fixed coset bug

--- arks/starks/stark101/src/parts/part_one.rs
+++ arks/starks/stark101/src/parts/part_one.rs
@@ -2,4 +2,17 @@
 //! - Low degree Extension
 //! - Commitment
 
-pub fn part_one() {}
+use polynomial::{ark_ff::PrimeField, evaluation::univariate::UnivariateEval};
+
+
+pub fn part_one<F: PrimeField>(a_s: &[F]) {
+    // Trace of the computation
+    // a_0, a_1, a_2, a_3,..., a_n
+    // 1, g, g^2, g^3,..., g^n
+    // f(g) = a;
+
+    let f_of_g = UnivariateEval::from_coefficients(a_s.to_vec());
+    
+    // Choosing the larger evaluation domain
+    
+}
